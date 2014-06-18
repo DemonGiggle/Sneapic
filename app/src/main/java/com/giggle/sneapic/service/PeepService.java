7179bb4973cad345cfd53ef3717dd9d72b857baa
Giggle Liao <giggle@zillians.com>
2014-06-18 19:26:10 +0800

    save image once photo is taken

--- app/src/main/java/com/giggle/sneapic/service/PeepService.java
+++ app/src/main/java/com/giggle/sneapic/service/PeepService.java
@@ -1,6 +1,7 @@
 package com.giggle.sneapic.service;
 
 import android.content.Intent;
+import android.os.Environment;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -9,13 +10,19 @@ import com.commonsware.cwac.wakeful.WakefulIntentService;
 import com.easycamera.DefaultEasyCamera;
 import com.easycamera.EasyCamera;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 /**
  * Created by giggle on 2014/6/18.
  */
 public class PeepService extends WakefulIntentService {
     private static final String TAG = "PeepService";
+
+    private File photoOuputFolder;
 
     private EasyCamera camera;
     private SurfaceHolder surface;
@@ -28,6 +35,19 @@ public class PeepService extends WakefulIntentService {
         public void onPictureTaken(byte[] data, EasyCamera.CameraActions actions) {
             Log.d(TAG, "Picture taken, size = " + data.length);
             camera.close();
+
+            //final Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            final SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd-HHmmss");
+            final String filename = formatter.format(System.currentTimeMillis());
+
+            try {
+                final FileOutputStream fos = new FileOutputStream(new File(photoOuputFolder, filename + ".jpg"));
+                fos.write(data);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     };
 
@@ -38,8 +58,16 @@ public class PeepService extends WakefulIntentService {
 
     @Override
     public void onCreate() {
-        Log.d(TAG, "service launched");
         super.onCreate();
+        Log.d(TAG, "service launched");
+        // create folder for
+        photoOuputFolder = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "SneaPic");
+        if (!photoOuputFolder.exists()) {
+            photoOuputFolder.mkdir();
+        }
+
         SurfaceView view = new SurfaceView(this);
         surface = view.getHolder();
         camera = DefaultEasyCamera.open();
